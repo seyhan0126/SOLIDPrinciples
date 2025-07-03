2d6fb20f1405d9642dafc248e47e05db5f496d6f
seyhan0126 <seyhan.mustafov@hotmail.de>
2025-07-03 11:16:22 +0300

    this way is violating DI principle

--- src/main/java/org/server/SOLID/DependencyInversion/DatabaseController.java
+++ src/main/java/org/server/SOLID/DependencyInversion/DatabaseController.java
@@ -1,11 +1,11 @@
 package org.server.SOLID.DependencyInversion;
 // the high level module
 public class DatabaseController {
-    private final MySQLDatabase database;
+    private final OracleDatabase database;
 
     public DatabaseController() {
         // tight binding when we program to a class (instead of an interface)
-        database = new MySQLDatabase();
+        database = new OracleDatabase();
         database.connect();
         database.disconnect();
     }
